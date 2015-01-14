d7b38221c51a3a6c21802da5d9ea9a159ce25bb2
Gregory Roussac <gregory.roussac@prestashop.com>
2015-01-14 15:42:31 +0100

    // PSCSX-2479

--- blocknewproducts.php
+++ blocknewproducts.php
@@ -59,20 +59,6 @@ class BlockNewProducts extends Module
 			&& $this->registerHook('displayHomeTabContent')
 		);
 
-		if ($success)
-		{
-			// Hook the module either on the left or right column
-			$theme = new Theme(Context::getContext()->shop->id_theme);
-			if ((!$theme->default_left_column || !$this->registerHook('leftColumn'))
-				&& (!$theme->default_right_column || !$this->registerHook('rightColumn')))
-			{
-				// If there are no colums implemented by the template, throw an error and uninstall the module
-				$this->_errors[] = $this->l('This module need to be hooked in a column and your theme does not implement one');
-				parent::uninstall();
-				return false;
-			}
-		}
-
 		$this->_clearCache('*');
 
 		return $success;
