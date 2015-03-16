6707d7d47a1a565d2c9f517225a706bcfafa01f3
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-03-16 16:43:16 +0100

    // Version updated to v1.9.5

--- blocknewproducts.php
+++ blocknewproducts.php
@@ -35,7 +35,7 @@ class BlockNewProducts extends Module
 	{
 		$this->name = 'blocknewproducts';
 		$this->tab = 'front_office_features';
-		$this->version = '1.9.4';
+		$this->version = '1.9.5';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
